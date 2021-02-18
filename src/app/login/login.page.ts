8dbe26fd0e57f2a2019b41ba1505247e94b40072
utkudogan <52001660+utkudogan@users.noreply.github.com>
2021-02-18 22:09:42 +0300

    worked on login page

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { FirebaseauthService } from '../firebaseauth.service';
+import { User } from '../user';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +10,70 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.page.scss'],
 })
 export class LoginPage implements OnInit {
+  userData: User = new User();
 
-  constructor() { }
+  constructor(
+    private firebaseauthService: FirebaseauthService,
+    private router: Router,
+    private alertController: AlertController
+  ) {}
 
-  ngOnInit() {
+  ngOnInit() {}
+
+  async giris() {
+    const kullanici = await this.firebaseauthService.EpostaParolaGirisYap(
+      this.userData
+    );
+    if (kullanici) {
+      console.log('Giriş Yapıldı! : ', kullanici);
+      this.router.navigateByUrl('/home');
+    }
   }
 
+  async sifreHatirlat(eposta): Promise<any> {
+    return await this.firebaseauthService
+      .sifreSifirla(eposta)
+      .then(() => true)
+      .catch(() => false);
+  }
+
+  async presentAlertPrompt() {
+    const alert = await this.alertController.create({
+      header: 'Şifre Hatırlat',
+      inputs: [
+        {
+          name: 'email',
+          type: 'text',
+          value: this.userData.email ? this.userData.email : '',
+          placeholder: this.userData.email ? '' : 'eposta adresinizi giriniz',
+        },
+      ],
+      buttons: [
+        {
+          text: 'Vazgeç',
+          role: 'cancel',
+          cssClass: 'secondary',
+          handler: () => {
+            console.log('Vazgeç');
+          },
+        },
+        {
+          text: 'Tamam',
+          handler: (value) => {
+            if (value.email.length >= 6) {
+              if (this.sifreHatirlat(value.email)) {
+                console.log(value.email + ' adresine gönderildi');
+              } else {
+                console.log('Şifre Sıfırlama Hatası');
+              }
+            } else {
+              console.log('Geçersiz e-posta adresi');
+            }
+          },
+        },
+      ],
+    });
+
+    await alert.present();
+  }
 }
